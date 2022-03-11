cfd773f2b3e1aad1b99642f0f2082a49f6f12a67
Duc-Hoang <hoanghuuduc777@gmail.com>
2022-03-11 14:19:00 +0700

    Huu Duc commit - finish add label Mat Khau, textbox Mat Khau

--- loginForm/loginForm/Form1.Designer.cs
+++ loginForm/loginForm/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.lblMatKhau = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -49,11 +51,30 @@
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "UserName";
             // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Location = new System.Drawing.Point(341, 148);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(268, 20);
+            this.txtMatKhau.TabIndex = 3;
+            this.txtMatKhau.Text = "Mật khẩu";
+            // 
+            // lblMatKhau
+            // 
+            this.lblMatKhau.AutoSize = true;
+            this.lblMatKhau.Location = new System.Drawing.Point(242, 151);
+            this.lblMatKhau.Name = "lblMatKhau";
+            this.lblMatKhau.Size = new System.Drawing.Size(52, 13);
+            this.lblMatKhau.TabIndex = 2;
+            this.lblMatKhau.Text = "Mật khẩu";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1177, 450);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.lblMatKhau);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -68,6 +89,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.Label lblMatKhau;
     }
 }
 
